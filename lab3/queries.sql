4f5abd1bc8e5357bfafd44a951b420a4f83810c8
velnck <v.veronika.sidorko@gmail.com>
2023-11-13 01:22:21 +0300

    lab 3

--- lab3/queries.sql
+++ lab3/queries.sql
@@ -2,50 +2,44 @@ SELECT * FROM books
 ORDER BY title LIMIT 5 OFFSET 10;
 
 
-SELECT * FROM genres
-ORDER BY name;
+SELECT * FROM books ORDER BY price;
 
 
-SELECT COUNT(id) AS reviews_quantity, review_author_id FROM reviews
-GROUP BY review_author_id;
+SELECT * FROM books 
+JOIN authors_books ON authors_books.book_id = books.id
+WHERE author_id = 4 
+AND price BETWEEN 17 AND 20;
 
 
-SELECT COUNT(id) FROM orders
-WHERE customer_id = 1;
+SELECT * FROM genres ORDER BY name;
 
 
-SELECT * FROM authors  ORDER BY name DESC;
-
-
-SELECT id, title, price FROM books
-WHERE price BETWEEN 15.00 AND 20.00 ORDER BY price;
+SELECT * FROM books
+WHERE publisher_id = 1;
 
 
 SELECT title, publisher_id, supplier_id FROM books
 WHERE publisher_id = 1 AND supplier_id = 3;
 
 
+SELECT COUNT(id) FROM orders
+WHERE customer_id = 1;
+
+
+SELECT * FROM authors  ORDER BY name;
+
+
+SELECT id, title, price FROM books
+WHERE price BETWEEN 15.00 AND 20.00 ORDER BY price;
+
+
+SELECT * FROM books
+WHERE title ILIKE '%история%';
+
+
 SELECT customer_id, delivery_address, creation_date::DATE FROM orders
 WHERE delivery_address LIKE '%ул. Брестская%';
 
 
-SELECT logs.id, users.username, message 
-FROM logs JOIN users 
-ON logs.user_id = users.id;
-
-
-SELECT COUNT(id) AS orders_quantity, customer_id 
-FROM orders
-GROUP BY customer_id
-HAVING COUNT(id) > 0;
-
-
-SELECT books.title, genre_id FROM genres_books
-JOIN books ON genres_books.book_id = books.id
-WHERE genre_id IN(2, 3);
-
-
-UPDATE books
-SET price = price + 5
+UPDATE books SET price = price + 5
 WHERE publisher_id = 3;
-
